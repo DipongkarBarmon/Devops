4afae16fa058ba449afc046a70e063c610a0b1dd
Dipongkar Barmon <dipongkorbarman02@gmail.com>
2026-01-20 22:48:41 +0600

    Hello Devops

---
+++ First/index.js
@@ -0,0 +1,14 @@
+const express =require('express')
+
+
+const app = express()
+
+
+app.get('/',(req,res)=>{
+    console.log('Correct')
+    res.send(`<h1>Hi There!</h1>`)
+})
+
+const port = process.env.PORT || 3000;
+
+app.listen(port,()=>{ console.log(`Server started at PORT ${port}`)})
